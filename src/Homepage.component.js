1b96e3c9d1b9f8460fb0c00cb999dab00af814dd
vivek.padelkar <vivek.padelkar@gmail.com>
2022-01-18 11:41:39 +0530

    craeted new project using npx and designed homepage

---
+++ src/Homepage.component.js
@@ -0,0 +1,46 @@
+import './homepage.style.scss'
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="menu-container">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Hats</h1>
+            <pspan className="subtitle">shop now</pspan>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">jackets</h1>
+            <pspan className="subtitle">shop now</pspan>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">sneakers</h1>
+            <pspan className="subtitle">shop now</pspan>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Women</h1>
+            <pspan className="subtitle">shop now</pspan>
+          </div>
+        </div>
+
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">Mens</h1>
+            <pspan className="subtitle">shop now</pspan>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
